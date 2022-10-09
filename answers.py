1fe19004f01d87c1c51a54196f78bbc5eddd03c1
charles <86170803+Charles-Wagaana@users.noreply.github.com>
2022-10-09 20:01:42 +0300

    Second Commit

--- answers.py
+++ answers.py
@@ -1,15 +1,19 @@
-with open('questions.txt', 'r+') as f:
+with open('questions.txt', 'w') as f:
     f.write('What is the capital city of Uganda?\n')
     f.write('Who is the current president of Uganda?\n')
     f.write('Who is the prime minister of Uganda?\n')
 
-    with open('answers.txt', 'r+') as a:
+with open('answers.txt', 'w') as a:
         a.write('Kampala\n')
         a.write('Yoweri Kaguta Museveni\n')
         a.write('Robina Nabbanja\n')
 
-        for q in f.readlines():
-        
-            for a in a.readlines():
-                print(q)
-                print(a)
+
+questions = open('questions.txt', 'r')
+answers = open('answers.txt', 'r')
+
+for q, a in zip(questions.readlines(), answers.readlines()):
+    print(q, a)
+
+
+
