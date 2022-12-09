35b3c06ffa85b6a33b08011c92dd278c153f1e64
Charles-Code <86170803+Charles-Wagaana@users.noreply.github.com>
2022-12-09 16:06:31 +0300

    Update hash.py

--- project_env/hash.py
+++ project_env/hash.py
@@ -2,7 +2,9 @@ import hashlib
 
 password = "mynewpassword"
 
-hashed_password = hashlib.sha512().hexdigest()
+password = password.encode(encoding='UTF-8', errors='strict')
+
+hashed_password = hashlib.sha512(password).hexdigest()
 
 print(hashed_password)
 
